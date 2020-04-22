006db357ef083a15f241b04416d088de0dfb9761
Christian Coulter <christiancoulter@Christians-MacBook-Air-2.local>
2020-04-22 12:43:49 -0600

    Finished setting up dependancies.

---
+++ onboarding-app/src/App.js
@@ -0,0 +1,17 @@
+import React, {useState, useEffect} from 'react';
+import './App.css';
+import Form from './Form';
+import * as yup from 'yup';
+import axios from 'axios';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      </header>
+      <Form />
+    </div>
+  );
+}
+
+export default App;
